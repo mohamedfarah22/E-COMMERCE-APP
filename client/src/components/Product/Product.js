d8a5aaba49476df80673ae8d51036c4c80ff6ad6
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-03 22:46:27 +1000

    styled and added product image

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,7 +1,10 @@
+import './product.css'
 function Product({product}){
     return(
-        <div className="product">
-            <p>{product}</p>
+        <div className="product-container">
+            
+            <img className="product-image" src={product.image_url}/>
+            <p className= "product-name">{product.product_name}</p>
         </div>
     )
 }
