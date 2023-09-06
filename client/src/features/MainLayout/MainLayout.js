4dd6c773f896bd257d600af2382490450273e35e
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-06 14:22:43 +1000

    added conditional rendering for cart component

--- client/src/features/MainLayout/MainLayout.js
+++ client/src/features/MainLayout/MainLayout.js
@@ -1,11 +1,18 @@
 import Header from "../Header/Header";
 import Categories from "../Categories/Categories";
 import Products from "../Products/Products";
+import CartPopUp from "../Cart/Cart";
+import { useState } from "react";
 function MainLayout({filterCategory,  setFilterCategory, setSelectedProduct}){
+    const [openPopUp, setOpenPopUp] = useState(false);
     return(
     <div classname="main-layout-container">
-        <Header />
+        <Header setOpenPopup = {setOpenPopUp} />
         <Categories filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
+        {openPopUp ?
+        <div className="cart-container">
+         <CartPopUp /> 
+        </div>: null}
         <Products filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} setSelectedProduct={setSelectedProduct} />
     </div>
     )
