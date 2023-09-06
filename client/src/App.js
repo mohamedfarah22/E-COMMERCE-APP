fb22a489124c70f76dfba745d0ce2cf12e77dfdb
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-06 15:25:21 +1000

    created user logged in state and userId state to remember cart items of logged in users

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,15 @@ import MainLayout from './features/MainLayout/MainLayout';
 function App() {
   const [filterCategory, setFilterCategory] = useState('All')
   const [selectedProduct, setSelectedProduct] = useState(null)
-  
+  const [loggedIn, setLoggedIn]  =useState(false); //to be passed into login page to authenticate user
+  const [userId, setUserId] = useState(null); //to be passed into login page to save user id.
 
  
   return (
     <div className="App">
   <Router>
     <Routes>
-      <Route path="/products/:product_name" element={<ProductPage selectedProduct = {selectedProduct} filterCategory = {filterCategory} setFilterCategory = {setFilterCategory}/>} />
+      <Route path="/products/:product_name" element={<ProductPage selectedProduct = {selectedProduct} filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>} />
       <Route path="/" element={<MainLayout filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} setSelectedProduct={setSelectedProduct}/>}/>
     </Routes>
    </Router>
