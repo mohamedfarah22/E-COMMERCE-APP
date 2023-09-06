cb1017150632f06d9af6053ed4ffcfb76be3099b
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-06 13:14:53 +1000

    added price to product page

--- client/src/features/ProductPage/Productpage.js
+++ client/src/features/ProductPage/Productpage.js
@@ -1,17 +1,25 @@
 import './ProductPage.css'
 import Header from '../Header/Header';
 import Categories from '../Categories/Categories';
+import CartPopUp from '../Cart/Cart';
+import { useState } from 'react';
 function ProductPage({selectedProduct, filterCategory, setFilterCategory}){
+    const [openPopUp, setOpenPopUp]  =useState(false);
     return(
     <div>
-        <Header />
+        <Header setOpenPopUp = {setOpenPopUp}/>
         <Categories filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
+        {openPopUp ?
+        <div className="cart-container">
+         <CartPopUp /> 
+        </div>: null}
         <div className='grid-product-page-container'>
         <div className="image-container">
             <img src= {selectedProduct.image_url}/>
         </div>
         <div className="product-container-page">
             <h3 className='product-name-page'>{selectedProduct.product_name}</h3>
+            <p className = "price-text">{`$${selectedProduct.price}`}</p>
             <button className = "add-to-cart-button">ADD TO CART</button>
             <p className = "description-header">Description</p>
             <p className="product-description-text">{selectedProduct.product_description}</p>
