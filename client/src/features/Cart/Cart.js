bceb1456ddd0262d7d594c4fd816631dab3167ea
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-06 14:47:07 +1000

    added close icon with css animations and cursor change on hover, clicking on icon also sets cart status to closed

--- client/src/features/Cart/Cart.js
+++ client/src/features/Cart/Cart.js
@@ -1,11 +1,19 @@
 import './cart.css';
 import closeLogo from '../../images/close-logo.png'
-function CartPopUp(){
+function CartPopUp({setOpenPopUp}){
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        setOpenPopUp(false)
+
+
+    }
     return (
         <div classname="popup-container">
         <div className="popup-body">
-        <div classname="close-text-container">
-            <p className="close-text">X</p>
+        <div classname="close-text-container" onClick = {onClickHandler}>
+        <span class="material-icons">
+            close
+        </span>
         </div>
             <h1>cart is open</h1>
            
