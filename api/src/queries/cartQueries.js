e9c1db2ffce169ca15b8bb05fe9a14349eb3c509
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-07 13:27:05 +1000

    added logic to only insert new row in cart if item is not already in database, if it is update quantity

--- api/src/queries/cartQueries.js
+++ api/src/queries/cartQueries.js
@@ -20,17 +20,33 @@ const getAllCarts = (req, res) => {
 
 //create a cart item
 const createCartItem = (req, res) => {
-    const {user_id, product_id, quantity} = req.body
-    
-    pool.query('INSERT INTO carts(user_id, product_id, quantity ) VALUES ($1, $2, $3) RETURNING *', [user_id, product_id, quantity], (error, results) => {
-        if(error){
-            res.status(400).send(error)
+    const { user_id, product_id, quantity } = req.body;
+
+    pool.query('SELECT * FROM carts WHERE user_id = $1 AND product_id = $2', [user_id, product_id], (error, results) => {
+        if (error) {
+            res.status(400).send(error);
+        } else if (results.rows.length === 0) {
+            // Cart item doesn't exist, insert a new one
+            pool.query('INSERT INTO carts (user_id, product_id, quantity) VALUES ($1, $2, $3) RETURNING *', [user_id, product_id, quantity], (error, insertResults) => {
+                if (error) {
+                    res.status(400).send(error);
+                }
+                res.status(201).send(`Cart item added with ID ${insertResults.rows[0].id}`);
+            });
+        } else {
+            // Cart item exists, update quantity
+            const existingQuantity = results.rows[0].quantity;
+            const newQuantity = existingQuantity + quantity;
+            pool.query('UPDATE carts SET quantity = $1 WHERE user_id = $2 AND product_id = $3', [newQuantity, user_id, product_id], (error) => {
+                if (error) {
+                    res.status(400).send(error);
+                }
+                res.status(200).send(`Cart item quantity updated`);
+            });
         }
-        res.status(201).send(`Cart added with ${results.rows[0].id}`)
+    });
+};
 
-    } )
-
-}
 
 const getUserCart = (req, res) => {
     const user_id = parseInt(req.params.user_id)
