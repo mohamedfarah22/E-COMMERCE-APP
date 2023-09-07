dd37305201116e1d24810e6872427663a549312b
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-07 11:37:16 +1000

    created cart state for cart items however number of items data not presisting across the shopping experience, will use the database to generate random ids for users not logged in to post cart to database and retrieve cart and create data presistence

--- client/src/features/Cart/Cart.js
+++ client/src/features/Cart/Cart.js
@@ -1,22 +1,49 @@
 import './cart.css';
-import closeLogo from '../../images/close-logo.png'
-function CartPopUp({setOpenPopUp}){
+import { useEffect, useState } from 'react';
+import CartCard from '../../components/CartCards/CartCard';
+function CartPopUp({setOpenPopUp, loggedIn, cartItems, cart, setCart}){
+   
     const onClickHandler = (e) => {
         e.preventDefault();
         setOpenPopUp(false)
+}
+const getCartTotal = (cart) => {
+    let total = 0;
+    cart.forEach((item) => {
+        total += (item.price * item.quantity)
+        
+    })
+    return total
+}
 
-
+useEffect(() => {
+    if(loggedIn){
+        //get cart items for logged in user
     }
+    const total = getCartTotal(cart)
+
+}, [loggedIn])
+
+
     return (
         <div classname="popup-container">
         <div className="popup-body">
-        <div classname="close-text-container" onClick = {onClickHandler}>
-        <span class="material-icons">
+        <div classname="close-text-container">
+            <span class="material-icons" onClick = {onClickHandler}>
             close
-        </span>
+             </span>
+            <h4 className="cart-heading">Cart</h4>
         </div>
-            <h1>cart is open</h1>
-           
+        <div className="cart-items-container">
+            {cartItems.map((cartItem) => {
+                return <CartCard cartItem = {cartItem} cart = {cart} setCart = {setCart}/>
+            })}
+        </div>
+        <div>
+
+        </div>
+       { getCartTotal(cart) ? <p>{getCartTotal(cart)}</p>: null}
+            
         </div>
 
         </div>
--- client/src/features/ProductPage/Productpage.js
+++ client/src/features/ProductPage/Productpage.js
@@ -2,16 +2,40 @@ import './ProductPage.css'
 import Header from '../Header/Header';
 import Categories from '../Categories/Categories';
 import CartPopUp from '../Cart/Cart';
-import { useState} from 'react';
-function ProductPage({selectedProduct, filterCategory, setFilterCategory}){
+import { useState, useEffect} from 'react';
+function ProductPage({selectedProduct, filterCategory, setFilterCategory, loggedIn, cartItems, setCartItems}){
     const [openPopUp, setOpenPopUp]  =useState(false);
+    const [cart, setCart] = useState([])
+    const onClickHandler = (e) => {
+        e.preventDefault();
+    if(cartItems.length === 0){
+        setCartItems([selectedProduct])
+    }
+    else{setCartItems([...cartItems, selectedProduct])}
+      if(loggedIn){
+        //logic to send cart items to database
+      }
+     
+      setOpenPopUp(true)
+    }
+
+    useEffect(() => {
+        cartItems.forEach((item) => {
+            if(cart.length === 0){
+                setCart([{id: item.id, price: item.price, quantity: 1}])
+            }
+            else{
+                setCart([...cart, {id: item.id, price: item.price, quantity: 1}])
+            }
+          })
+    }, [cartItems])
     return(
     <div>
         <Header setOpenPopUp = {setOpenPopUp}/>
         <Categories filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
         {openPopUp ?
         <div className="cart-container">
-         <CartPopUp setOpenPopUp = {setOpenPopUp} /> 
+         <CartPopUp setOpenPopUp = {setOpenPopUp} loggedIn = {loggedIn} cartItems = {cartItems} setCartItems={setCartItems} cart = {cart} setCart = {setCart}/> 
         </div>: null}
         <div className='grid-product-page-container'>
         <div className="image-container">
@@ -20,7 +44,7 @@ function ProductPage({selectedProduct, filterCategory, setFilterCategory}){
         <div className="product-container-page">
             <h3 className='product-name-page'>{selectedProduct.product_name}</h3>
             <p className = "price-text">{`$${selectedProduct.price}`}</p>
-            <button className = "add-to-cart-button">ADD TO CART</button>
+            <button onClick = {onClickHandler} className = "add-to-cart-button">ADD TO CART</button>
             <p className = "description-header">Description</p>
             <p className="product-description-text">{selectedProduct.product_description}</p>
         </div>
