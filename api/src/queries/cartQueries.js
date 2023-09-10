53902e567e17eada78f33d3db8f67260a321a4ed
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-10 20:39:28 +1000

    added deleteCartItem, updatequyantitycart, getusercart and get cart total

--- api/src/queries/cartQueries.js
+++ api/src/queries/cartQueries.js
@@ -20,7 +20,7 @@ const getAllCarts = (req, res) => {
 
 //create a cart item
 const createCartItem = (req, res) => {
-    const { user_id, product_id, quantity } = req.body;
+    const { user_id, product_id, quantity} = req.body;
 
     pool.query('SELECT * FROM carts WHERE user_id = $1 AND product_id = $2', [user_id, product_id], (error, results) => {
         if (error) {
@@ -41,7 +41,7 @@ const createCartItem = (req, res) => {
                 if (error) {
                     res.status(400).send(error);
                 }
-                res.status(200).send(`Cart item quantity updated`);
+                res.status(200).send(String(newQuantity));
             });
         }
     });
@@ -49,7 +49,7 @@ const createCartItem = (req, res) => {
 
 
 const getUserCart = (req, res) => {
-    const user_id = parseInt(req.params.user_id)
+    const user_id = req.params.user_id
 
     pool.query('SELECT * FROM carts WHERE user_id = $1', [user_id], (error, results) => {
         if(error){
@@ -58,8 +58,69 @@ const getUserCart = (req, res) => {
         res.status(200).send(results.rows)
     })
 }
+//delete cart item
+const deleteCartItem = (req, res) => {
+    const {user_id, product_id} = req.query
+    pool.query('DELETE FROM carts WHERE user_id = $1 AND product_id = $2 RETURNING *', [user_id, product_id], (error, results) => {
+        if(error){
+            res.status(400).send(error)
+        }
+        res.status(200).json(results.rows)
+    })
+}
+//get user and product cart
+const getUserProductCart = (req, res) => {
+    const { user_id, product_id } = req.query;
+
+    pool.query('SELECT * FROM carts WHERE user_id = $1 AND product_id = $2', [user_id, product_id], (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(400).send(error);
+            return;
+        }
+
+        if (results && results.rows) {
+            res.status(200).json(results.rows);
+        } else {
+            res.status(404).json({ message: 'No matching rows found' });
+        }
+    });
+};
+
+//update quanttiy of product
+const updateQuantityCartItem = (req, res) => {
+    const {user_id, product_id, quantity} = req.query
+    parseInt(quantity)
+    pool.query('UPDATE carts SET quantity = $1 WHERE user_id = $2 AND product_id = $3', [quantity, user_id, product_id], (error,results) => {
+        if(error){
+            res.status(400).send(error)
+        }
+        res.status(200).send("Update Successful")
+})
+}
+const getCartTotal = (req, res) => {
+    const { user_id, product_id } = req.query;
+  
+    pool.query(
+      'SELECT SUM(quantity * price) AS total_cost FROM carts JOIN products ON carts.product_id = products.id WHERE carts.user_id = $1',
+      [user_id],
+      (error, results) => {
+        if (error) {
+          res.status(400).send(error);
+        } else {
+          const totalCost = results.rows[0].total_cost;
+          res.status(200).json({ total_cost: totalCost });
+        }
+      }
+    );
+  };
+  
 module.exports ={
     getAllCarts, 
     createCartItem,
-    getUserCart
+    getUserCart,
+    deleteCartItem,
+    updateQuantityCartItem,
+    getUserProductCart,
+    getCartTotal
 }
