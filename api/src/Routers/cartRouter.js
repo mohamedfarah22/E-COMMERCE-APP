b5605e9408a25d62a2e1caba15c94357700cd45d
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-10 20:40:11 +1000

    added database queries to paths

--- api/src/Routers/cartRouter.js
+++ api/src/Routers/cartRouter.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const db = require('../queries/cartQueries.js')
 
 //munt get all carts func on cart router
-router.get('/', db.getAllCarts)
+router.get('/all', db.getAllCarts)
+router.get('/user-product-queries', db.getUserProductCart)
+router.get('/cart-total', db.getCartTotal)
 router.post('/', db.createCartItem)
 router.get('/:user_id',db.getUserCart)
+router.delete('/', db.deleteCartItem)
+router.put('/', db.updateQuantityCartItem)
+
 module.exports = router
