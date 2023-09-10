f990ac0adb389b34f9025394821b7f37938c8831
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-10 20:04:20 +1000

    managed selected product that is rendered by productPage in global state and is being set by axios call from db to create data presistence on refresh

--- client/src/features/ProductPage/Productpage.js
+++ client/src/features/ProductPage/Productpage.js
@@ -4,28 +4,55 @@ import Categories from '../Categories/Categories';
 import CartPopUp from '../Cart/Cart';
 import { useState, useEffect} from 'react';
 import axios from 'axios';
-function ProductPage({selectedProduct, filterCategory, setFilterCategory, userId}){
-    const [openPopUp, setOpenPopUp]  =useState(false);
-    const [cart, setCart] = useState([])
-    
+import { useSelectedProduct } from '../Products/ProductsContext';
+import { useParams } from 'react-router-dom';
+import { usePopup } from '../Cart/CartPopUpContext';
+import { useCart } from '../Cart/CartContext';
+function ProductPage({filterCategory, setFilterCategory, userId}){
+    const {cart, setCart} = useCart()
+    const {openPopUp, setOpenPopUp} = usePopup()
+    const { selectedProduct, setSelectedProduct } = useSelectedProduct();
+    const {product_id} = useParams();
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+      if(selectedProduct === null || selectedProduct === undefined){
+          axios.get(`http://localhost:4000/products/${product_id}`).then((response) => {
+               setLoading(true)
+                setSelectedProduct(response.data)
+                setLoading(false)
+          }).catch((error) => {
+            console.log(error)
+            setLoading(false)
+          })
+      }
+  
+      }, [])
+     
     const onClickHandler = (e) => {
         e.preventDefault();
+    if(selectedProduct){
         axios.post("http://localhost:4000/carts", {
             user_id: userId,
-            product_id: selectedProduct.id, quantity: 1
+            product_id: selectedProduct[0].id, quantity: 1
         }).then((response) => {
             setCart([...cart, {
                 user_id: userId,
-                product_id: selectedProduct.id,
-                quantity: response.data
+                product_id: selectedProduct[0].id,
+                quantity: parseInt(response.data)
             }])
             console.log(response.data)
         })
-     
+    }
      
       setOpenPopUp(true)
     }
-
+    if (loading) {
+        //render a loading indicator here
+        return <p>Loading...</p>;
+      }
+      if (!selectedProduct) {
+        return null; // or render an error message
+      }
    
     return(
     <div>
@@ -33,18 +60,18 @@ function ProductPage({selectedProduct, filterCategory, setFilterCategory, userId
         <Categories filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
         {openPopUp ?
         <div className="cart-container">
-         <CartPopUp openPopUp = {openPopUp}setOpenPopUp = {setOpenPopUp} cart = {cart} setCart = {setCart} userId = {userId}/> 
+         <CartPopUp userId = {userId}/> 
         </div>: null}
         <div className='grid-product-page-container'>
         <div className="image-container">
-            <img src= {selectedProduct.image_url}/>
+            <img src= {selectedProduct[0].image_url}/>
         </div>
         <div className="product-container-page">
-            <h3 className='product-name-page'>{selectedProduct.product_name}</h3>
-            <p className = "price-text">{`$${selectedProduct.price}`}</p>
+            <h3 className='product-name-page'>{selectedProduct[0].product_name}</h3>
+            <p className = "price-text">{`$${selectedProduct[0].price}`}</p>
             <button onClick = {onClickHandler} className = "add-to-cart-button">ADD TO CART</button>
             <p className = "description-header">Description</p>
-            <p className="product-description-text">{selectedProduct.product_description}</p>
+            <p className="product-description-text">{selectedProduct[0].product_description}</p>
         </div>
         </div>
     </div>
