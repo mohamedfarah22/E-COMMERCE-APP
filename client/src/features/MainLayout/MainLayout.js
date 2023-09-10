e53e779c165682ca1b5ffea53a759010a0c80816
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-10 20:01:20 +1000

    removed prop drilling of state setters and states that are now managed globally by context

--- client/src/features/MainLayout/MainLayout.js
+++ client/src/features/MainLayout/MainLayout.js
@@ -2,18 +2,18 @@ import Header from "../Header/Header";
 import Categories from "../Categories/Categories";
 import Products from "../Products/Products";
 import CartPopUp from "../Cart/Cart";
-import { useState } from "react";
-function MainLayout({filterCategory,  setFilterCategory, setSelectedProduct}){
-    const [openPopUp, setOpenPopUp] = useState(false);
+import { usePopup } from '../Cart/CartPopUpContext';
+function MainLayout({filterCategory,  setFilterCategory, userId}){
+    const {openPopUp, setOpenPopUp }= usePopup();
     return(
     <div classname="main-layout-container">
         <Header setOpenPopup = {setOpenPopUp} />
         <Categories filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
         {openPopUp ?
         <div className="cart-container">
-         <CartPopUp /> 
+         <CartPopUp userId={userId} /> 
         </div>: null}
-        <Products filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} setSelectedProduct={setSelectedProduct} />
+        <Products filterCategory = {filterCategory} setFilterCategory = {setFilterCategory} />
     </div>
     )
 }
