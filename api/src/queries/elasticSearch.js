dc7afd20f0fd436a33251546670d8f217b79132e
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-09-19 18:13:50 +1000

    created bulk indexing function to move products to elasticsearch db, created function to create index and created function to search elastic search based on inputed query

---
+++ api/src/queries/elasticSearch.js
@@ -0,0 +1,135 @@
+//import elasticsearch client
+const {Client} = require('@elastic/elasticsearch');
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:"admin",
+    password: "ecommdb",
+    host: "ecomm-database-postgres",
+    database: "ecommercedatabase",
+    port: 5432
+});
+//create an elasticsearch client instance
+const esClient = new Client({node: 'http://elastic-search:9200'})
+//check if index exists then drop index
+async function checkAndDropIndex(indexName) {
+    try {
+      // Check if the index exists
+      const { body: existsResponse } = await esClient.indices.exists({
+        index: indexName,
+      });
+  
+      if (existsResponse) {
+        // Index exists; drop (delete) it
+        const { body: dropResponse } = await esClient.indices.delete({
+          index: indexName,
+        });
+  
+        if (dropResponse.acknowledged) {
+          console.log(`Index '${indexName}' deleted successfully.`);
+        } else {
+          console.error(`Failed to delete index '${indexName}'.`);
+        }
+      } else {
+        console.log(`Index '${indexName}' does not exist.`);
+      }
+    } catch (error) {
+      console.error(`Error checking and dropping index '${indexName}':`, error);
+    }
+  }
+  
+
+
+//define products index in elastic search
+async function createIndex(){
+    try{
+        //delete index
+        await checkAndDropIndex('product_index')
+        await esClient.indices.create({
+            index: 'product_index',
+            body: {
+                mappings:{
+                    properties:{
+                        product_name: {type: 'text'},
+                        product_description: {type: 'text'},
+                        category: {type: 'keyword'},
+                        price: {type: 'float'}, 
+                        available_quantities: {type: 'integer'},
+                        image_url: {type: 'text'}
+                    }
+                }
+            }
+        });
+    console.log('index created successfully')
+
+    } catch(error){
+        console.error('Error creating index: ', error)
+
+    }
+}
+//adds data from products table to elastic search
+async function indexProductData(){
+    try{
+        const productData = await pool.query('SELECT * FROM products');
+        const products = productData.rows;
+        //array to store bulk indexing
+        const bulkBody = [];
+
+        //loop through products and create index reqs
+        products.forEach((product) => {
+            bulkBody.push(
+                {index: {_index:'product_index', _id: product.id}},
+                {
+                    product_name: product.product_name,
+                    product_description: product.product_description,
+                    category: product.category,
+                    price: product.price,
+                    available_quantity: product.available_quantity,
+                    image_url: product.image_url
+                }
+
+            );
+        });
+        
+        //use elasticsearch client to perform bulk indexing
+        const {body: bulkResponse} = await esClient.bulk({ refresh: true, body: bulkBody });
+        
+        if(bulkResponse.errors){
+            console.error('Failed to index some products', bulkResponse.items);
+        } else{
+            console.log(`Indexed ${products.length} products`)
+        }
+    
+
+    }catch(error){
+        console.error('Error indexing products:', error)
+
+    }
+}
+
+//search function
+async function searchProducts(searchPhrase){
+  try{
+    const body = await esClient.search({
+      index: 'product_index',
+      body: {
+        query:{
+          multi_match:{
+            query: searchPhrase,
+            fields: ["product_name", "product_description", "category"]
+
+          },
+        },
+      },
+    });
+
+  return body.hits.hits
+  } catch(error){
+    console.log('Error searching documents: ', error)
+
+  }
+} 
+module.exports={
+    createIndex,
+    indexProductData, 
+    searchProducts
+}
