c64fa2f6ba5681da6a6df2b74c3542ce2c345539
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-10-02 13:31:42 +1100

    added navigation tests for header tests on company name and login / log out

--- client/src/tests/Header.test.js
+++ client/src/tests/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen} from '@testing-library/react';
+import { render, screen, waitFor} from '@testing-library/react';
 import Header from '../features/Header/Header.js';
 import { MemoryRouter } from 'react-router-dom';
 import { CartProviderPopUp } from '../features/Cart/CartPopUpContext.js';
@@ -8,6 +8,8 @@ import { ProductsProvider } from '../features/Products/ProductsContext.js';
 import { cartQuantityCalculator } from '../features/Header/HeaderHelperFunctions.js';
 import '@testing-library/jest-dom';
 import { server } from './mocks/server';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
 beforeAll(() => server.listen());
 
 // Reset any request handlers that we may add during the tests,
@@ -16,6 +18,11 @@ afterEach(() => server.resetHandlers());
 
 // Clean up after the tests are finished.
 afterAll(() => server.close());
+//mock navigte
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // Use the actual module except for useNavigate
+  useNavigate: jest.fn(), // Mock useNavigate
+}));
 describe('Header component initial render' , () =>{
 
 test('renders Header with correct text in banner, company name and log in text', () => {
@@ -108,4 +115,102 @@ test('renders Header with correct text in banner, company name and log in text',
          
         }) 
 
-     
+  //test luxuria navigation
+ test('luxuria heading navigates to home/dashboard', async () => {
+  const navigateMock = jest.fn();
+    
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+  render(
+        
+    <MemoryRouter>
+      <CartProviderPopUp>
+        <CartProvider>
+          <ProductsProvider>
+            <Header />
+          </ProductsProvider>
+        </CartProvider>
+      </CartProviderPopUp>
+    </MemoryRouter>
+    );  
+
+    //set up user events
+    const user = userEvent.setup();
+   await  waitFor(() => {
+       //find heading
+    const luxuriaHeading = screen.getByRole('heading', {name: 'Luxuria'})
+
+    //click heading
+    user.click(luxuriaHeading);
+
+    //navigate called with /
+    expect(navigateMock).toHaveBeenCalledWith('/')
+    })
+   
+ })
+  //test login navigation
+test('on click login navigates to login page', async () => {
+  const navigateMock = jest.fn();
+    
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+  render(
+        
+    <MemoryRouter>
+      <CartProviderPopUp>
+        <CartProvider>
+          <ProductsProvider>
+            <Header />
+          </ProductsProvider>
+        </CartProvider>
+      </CartProviderPopUp>
+    </MemoryRouter>
+    );  
+    const user = userEvent.setup();
+   await  waitFor(() => {
+       //find log in text
+    const logInText= screen.getByText('Log In')
+
+    //click heading
+    user.click(logInText);
+
+    //navigate called with /
+    expect(navigateMock).toHaveBeenCalledWith('/login')
+    })
+})
+  //test log out navigation
+  test('on click logout navigates to login page', async () => {
+    server.use(
+      rest.post('http://localhost:4000/auth/logout', (req, res, ctx) => {
+          return res(ctx.status(200))
+      })
+  )
+    const navigateMock = jest.fn();
+    const setLoggedIn = jest.fn()
+      
+      require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+    render(
+          
+      <MemoryRouter>
+        <CartProviderPopUp>
+          <CartProvider>
+            <ProductsProvider>
+              <Header loggedIn = {true} setLoggedIn={setLoggedIn}/>
+            </ProductsProvider>
+          </CartProvider>
+        </CartProviderPopUp>
+      </MemoryRouter>
+      );  
+      const user = userEvent.setup();
+     await  waitFor(() => {
+         //find log in text
+      const logOutText= screen.getByText('Log Out')
+  
+      //click heading
+      user.click(logOutText);
+  
+      //navigate called with /login
+      expect(navigateMock).toHaveBeenCalledWith('/login');
+      //logged in set to false
+      expect(setLoggedIn).toHaveBeenCalledWith(false)
+      })
+  })
+  
