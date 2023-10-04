71a34ea6bc53affb2b2f42f675e2f02f604c243c
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-10-04 17:17:25 +1100

    refactored products router and created success path for products router test suite also added docker-compose file to create test db

--- api/server.js
+++ api/server.js
@@ -5,11 +5,13 @@ const port = 4000
 const cors = require('cors');
 const passport = require('passport')
 const session = require('express-session')
+const pool  = require('./dbConfig')
 //require elastic search functions
 const elasticSearch = require('./src/queries/elasticSearch')
+
 //require products router
 
-const productsRouter = require('./src/Routers/productsRouter.js')
+const productsRouter = require('./src/Routers/productsRouter.js')(pool)
 const authRouter = require('./src/Routers/authRouter.js')
 const usersRouter = require('./src/Routers/usersRouter.js')
 const cartsRouter = require('./src/Routers/cartRouter.js')
@@ -19,6 +21,7 @@ const searchRouter = require('./src/Routers/searchRouter.js')
 app.use(cors());
 //use body parser
 app.use(bodyParser.json())
+//attach db to app
 
 //create express session
 app.use(
@@ -35,8 +38,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 //create index and bulk updated index in elasticsearch with product data
+//do not index products to elastic search if node_env is test
+if (process.env['NODE_ENV'] !== 'test') {
 elasticSearch.createIndex().then(() => elasticSearch.indexProductData()).catch(error => console.error("Error: ", error));
-
+}
 
 //Mount the router at products path
 
@@ -58,9 +63,13 @@ app.use('/check-out', stripeRouter)
 
 //mount search router
 app.use('/search', searchRouter)
-app.listen(port, () => {
-  console.log(`E-commerce app listening on ${port}`)
-})
+//make sure node_env is not test before allowing express to listen
+if (process.env['NODE_ENV'] !== 'test') {
+  app.listen(port, () => {
+    console.log(`E-commerce app listening on ${port}`)
+  })
+  
+}
 
 //export app 
 
--- api/src/Routers/productsRouter.js
+++ api/src/Routers/productsRouter.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../queries/productQueries')
-const Pool = require('pg').Pool;
-
-const pool = new Pool({
-    user:"admin",
-    password: "ecommdb",
-    host: "ecomm-database-postgres",
-    database: "ecommercedatabase",
-    port: 5432
-});
 
 
 //get all products
+module.exports = (pool) => {
 router.get('/', (req, res, next) => {
     if (req.query.category) {
       // Handle getting products by category these functions work better within the router not sure why
       const category = req.query.category
-
       pool.query('SELECT * FROM products WHERE category= $1', [category], (error, results) => {
           if(error){
             res.status(500).json({ error: "Internal server error" });
-  
+            console.log(error)
           } else{
           res.status(200).json(results.rows)
           }
@@ -42,25 +33,96 @@ router.get('/', (req, res, next) => {
   
 
 //get all categories
-router.get('/categories', db.getAllCategories(pool))
+router.get('/categories', (req, res, next) => {
+  pool.query('SELECT DISTINCT category FROM products', (error, results) => {
+    if(error){
+        res.status(500).json({ error: "Internal server error" });
+    }
+    res.status(200).send(results.rows)
+})
+})
 
 
 //get products by id
 
-router.get('/:id', db.getProductById(pool))
+router.get('/:id', (req, res, next) => {
+    const id =  parseInt(req.params.id);
+    pool.query('SELECT * FROM products WHERE id=$1', [id], (error, results) => {
+        if(error){
+            res.status(500).json({ error: "Internal server error" });
+        } else{
+          if (results.rows.length === 0) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(200).json(results.rows);
+        }
+          
+    }
+  })
+
+})
 
 
-//get products by category
 
 
 //create new product
-router.post('/', db.createProduct(pool))
+router.post('/', (req, res, next) => {
+  const {product_name, product_description, category, price, available_quantity, image_url} = req.body;
+
+    pool.query('INSERT INTO products (product_name, product_description, category, price, available_quantity, image_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [product_name, product_description, category, price, available_quantity, image_url], (error, results) => {
+       if(error){
+        res.status(500).json({ error: "Internal server error" });
+       } 
+
+       res.status(201).json(results.rows[0])
+    })
+
+})
 
 //updated existing product
-router.put('/:id', db.updateProduct(pool))
+router.put('/:id', (req, res, next) => {
+  const id = parseInt(req.params.id)
+  const {product_name, product_description, price, available_quantity} = req.body;
+
+      pool.query(
+          'UPDATE products SET product_name=$1, product_description=$2, price=$3, available_quantity=$4 WHERE id=$5 RETURNING *',
+          [product_name, product_description, price, available_quantity, id],
+          (error, results) => {
+              if (error) {
+                  console.error("Error updating product:", error);
+                  res.status(500).json({ error: "Internal server error" });
+              } else {
+                  if (results.rows.length === 0) {
+                      res.status(404).json({ error: "Product not found" });
+                  } else {
+                      res.status(200).json(results.rows[0]);
+                  }
+              }
+          }
+      );
+
+})
+
 
 //delete existing product
-router.delete('/:id', db.deleteProduct(pool))
+router.delete('/:id',(req, res, next) => {
+  const id = parseInt(req.params.id)
+    
+  pool.query('DELETE FROM products WHERE id=$1 RETURNING *', [id], (error, results) => {
+      if(error){
+          res.status(500).json({ error: "Internal server error" });
+      }else {
+        if (results.rows.length === 0) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+          res.status(200).json({ message: `Product deleted with ID: ${id}` })
+        }
+    }
+
+  })
+})
+
+return router
+}
 
 
-module.exports = router;
