67ac9140f0a1f57c96f93a7ce1d2ff64ed7a375f
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-10-08 17:57:27 +1100

    removed pool from routers

--- api/server.js
+++ api/server.js
@@ -49,10 +49,9 @@ VALUES
 ('Chic Gold Choker Necklace', 'Elevate your neckline with this chic gold choker necklace, weighing 6 grams. Its modern design and comfortable fit make it an ideal accessory for both casual and formal looks.', 'necklaces', 449.99, 15, './necklaces.jpeg'),
 ('Classic Gold Band Ring', 'Embrace timeless elegance with this classic gold band ring, weighing 6 grams. Its simple yet sophisticated design makes it a versatile accessory for any occasion.', 'rings', 499.99, 15, './rings.jpeg'),
 ('Sparkling Gold Halo Ring', 'Radiate brilliance with this diamond halo ring, weighing 4.5 grams. The dazzling center diamond is surrounded by a halo of smaller diamonds for maximum sparkle.', 'rings', 899.99, 8, './rings.jpeg'),
-('Delicate Gold Stackable Ring', 'Create your own unique stack with this delicate gold ring, weighing 2 grams. Its slim design allows you to mix and match for a personalized look.', 'rings', 249.99, 20, './rings.jpeg')
+('Delicate Gold Stackable Ring', 'Create your own unique stack with this delicate gold ring, weighing 2 grams. Its slim design allows you to mix and match for a personalized look.', 'rings', 249.99, 20, './rings.jpeg');
 `)
 }
-dbProductIniti()
 //create express session
 app.use(
   session({
