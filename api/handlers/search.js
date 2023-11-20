d865c409ebc00d772be43eb9a4b0b091621166b2
Mohamed Farah <mohamedfarah@Mohameds-MBP.modem>
2023-11-20 17:36:37 +1100

    added search lambdas

--- api/handlers/search.js
+++ api/handlers/search.js
@@ -1,208 +1,157 @@
-const {Client} = require('@elastic/elasticsearch');
-const AWS = require('aws-sdk');
-const axios = require('axios');
-const {aws4Interceptor} = require("aws4-axios")
-const connection = require('http-aws-es')
-
-
-
+const aws4 = require('aws4');
 const Pool = require('pg').Pool;
-const fetch = require('node-fetch-commonjs')
+const https = require('https');
+require('dotenv').config();
 
 const pool = new Pool({
     user: 'masteruser',
-    password: "ecommdbpw",
+    password: 'ecommdbpw',
     host: 'ecommdb.covnxnivrrc5.ap-southeast-2.rds.amazonaws.com',
-    database: "ecommdb",
+    database: 'ecommdb',
     port: 5432,
     ssl: true
-})
-//es url
-const url = 'https://search-product-search-3rxti5gq3imvvpyqxa5jub5aua.ap-southeast-2.es.amazonaws.com/';
+});
 
-//function to check if elastic search index exists
-async function checkIfEsIndexExists(indexName, url){
-    
+module.exports.bulkIndexProductData = async (event) => {
     try {
-        const response = await fetch(url, { method: 'HEAD' });
-    
-        if (response.ok) {
-          return true
-          
-        } else {
-          console.error(`Error: HTTP ${response.status} ${response.statusText}`);
-        }
-      } 
-    catch (error) {
-        throw new Error(error)
-       
-      }
-    }
-    
-//search function for es 2.3
-async function searchProducts(searchPhrase){
-    try{
-      const body = await esClient.search({
-        index: 'product_index',
-        body: {
-          query:{
-            multi_match:{
-              query: searchPhrase,
-              fields: ["product_name", "product_description", "category"]
-  
-            },
-          },
-        },
-      });
-  
-    return body.hits.hits
-    } catch(error){
-        throw new Error('Error searching documents '+ error.message)
-      
-  
-    }
-  } 
-//function to create index
+        const productData = await pool.query('SELECT * FROM products');
+        const products = productData.rows;
+        const bulkBody = [];
 
-async function createIndex(indexName) {
-    try {
-
-        await esClient.indices.create({
-            index: indexName,
-            body: {
-                settings: {
-                    index: {
-                        number_of_shards: 1,
-                        number_of_replicas: 1
-                    }
-                },
-                mappings: {
-                    properties: {
-                        product_name: { type: 'text' },
-                        product_description: { type: 'text' },
-                        category: { type: 'keyword' },
-                        price: { type: 'float' },
-                        available_quantities: { type: 'integer' },
-                        image_url: { type: 'text' }
-                    }
+        // loop through products and create index reqs
+        products.forEach((product) => {
+            bulkBody.push(
+                { index: { _index: 'product_index', _id: product.id } },
+                {
+                    product_name: product.product_name,
+                    product_description: product.product_description,
+                    category: product.category,
+                    price: product.price,
+                    available_quantity: product.available_quantity,
+                    image_url: product.image_url
                 }
-            }
+            );
         });
-        return true;
+
+        const requestBody = bulkBody.map(JSON.stringify).join('\n') + '\n';
+
+        const requestOptions = aws4.sign({
+            hostname: 'search-product-search-3rxti5gq3imvvpyqxa5jub5aua.ap-southeast-2.es.amazonaws.com',
+            service: 'es',
+            region: 'ap-southeast-2',
+            method: 'POST',
+            path: '/product_index/_bulk',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: requestBody
+        }, {
+            accessKeyId: 'AKIAVAPEO6NFPIXWWJNQ',
+            secretAccessKey: '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',
+        });
+
+        // Make the HTTP request
+        const response = await new Promise((resolve, reject) => {
+            const req = https.request(requestOptions, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve(data);
+                });
+            });
+
+            req.on('error', (error) => {
+                reject(error);
+            });
+
+            req.write(requestOptions.body);
+            req.end();
+        });
+
+        console.log(response);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ response: response }),
+        };
     } catch (error) {
-        throw new Error('Error creating index: ' + error.message);
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: error.message })
+        };
     }
-}
-//adds data from products table to elastic search
-async function indexProductDataFromSQL(pool) {
-    try {
-      // Fetch product data from the SQL database
-      const productData = await pool.query('SELECT * FROM products');
-      const products = productData.rows;
-  
-      // Prepare the bulk request body
-      let bulkRequestBody = '';
-  
-      products.forEach((product) => {
-        bulkRequestBody += JSON.stringify({ index: { _index: 'test', _id: product.id } }) + '\n';
-        bulkRequestBody += JSON.stringify({
-          product_name: product.product_name,
-          product_description: product.product_description,
-          category: product.category,
-          price: product.price,
-          available_quantity: product.available_quantity,
-          image_url: product.image_url,
-        }) + '\n';
-      });
-  
-      const { body: response } = await esClient.bulk({ refresh: true, body: bulkRequestBody });
-      
-      if (response.errors) {
-        throw new Error('Failed to index some products', response.items);
-      } else {
-        return true;
+};
+
+module.exports.search = async(event) => {
+  try {
+    const query = event.queryStringParameters && event.queryStringParameters.query
+    if(!query){
+      return{
+        statusCode: 404,
+        body: JSON.stringify({message: 'please type in a search query'})
       }
-    } catch (error) {
-      throw new Error('Error indexing products: ' + error.message);
-    }
-  }
+    }else {
+      console.log( process.env)
+      const requestOptions = aws4.sign({
+        hostname: 'search-product-search-3rxti5gq3imvvpyqxa5jub5aua.ap-southeast-2.es.amazonaws.com',
+        service: 'es',
+        region: 'ap-southeast-2',
+        method: 'GET',
+        path: `/product_index/_search?q=${query}`,
+    }, {
+      accessKeyId: 'AKIAVAPEO6NFPIXWWJNQ',
+      secretAccessKey: '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',
+    });
+    const response = await new Promise((resolve, reject) => {
+      const req = https.request(requestOptions, (res) => {
+          let data = '';
+
+          res.on('data', (chunk) => {
+              data += chunk;
+          });
+
+          res.on('end', () => {
+              resolve(data);
+          });
+      });
+
+      req.on('error', (error) => {
+          reject(error);
+      });
+
+      req.end();
+  });
+
   
+  const parsedResponse = JSON.parse(response);
+  console.log('Search Response:', parsedResponse.hits.hits);
 
-module.exports.createMappingForIndex = async (event) => {
-    //check if index exists
-    try {
-        const indexExists = await checkIfEsIndexExists('product_index', url);
 
-        if(!indexExists){
-            try {
-                const indexCreated = await createIndex('product_index');
-                if(indexCreated){
-                    return{
-                        statusCode: 200,
-                        body: JSON.stringify({message: 'Index successfully created'})
-                    }
-                }
-            } catch (error) {
-                return{
-                    statusCode: 500,
-                    body: JSON.stringify({message: error.message})
-                }
-            }
-           
-    
-        }
-        
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({error: error.message})
-        }
-        
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+     
+    },
+      statusCode: 200,
+      body: JSON.stringify(parsedResponse.hits.hits),
+  };
     }
-   
-}
-module.exports.bulkProductDatatToES = async (event) => {
- try {
-    const indexedProductData = await indexProductDataFromSQL(pool);
-    if(indexedProductData){
-        return {
-            statusCode: 200,
-            body: {message: 'product data index successful'}
-        }
-    }
-
     
- } catch (error) {
+  } catch(error){
     return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'product data index successful' })
-    }
- }
-}
-module.exports.search = async (event) => {
-    try {
-    const searchQuery = event.queryStringParameters && event.queryStringParameters.q
-    if(!searchQuery){
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: 'please provide search query'})
-        }
-    }
-    else{
-        const results = await searchProducts(searchQuery);
-        return {
-            statusCode: 200,
-            body: JSON.stringify(results)
-        }
+      headers: {
+          'Access-Control-Allow-Origin': '*',
+         
+        },
+      statusCode: 500,
+      body: JSON.stringify({error: error})
+  }
 
-    }
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({error: error.message})
-        }
-        
-        
-    }
 
-}
+  }
+} 
+
+
---
+++ api/s_getUserProductQueriesLambda 2.js
@@ -0,0 +1,29 @@
+
+var serverlessSDK = require('./serverless_sdk/index.js');
+serverlessSDK = new serverlessSDK({
+  orgId: 'mohamedfarah',
+  applicationName: 'ecomm-app-api',
+  appUid: 'sMQdTV1LnpWvDXxRBv',
+  orgUid: 'f94eafca-77b9-4daa-9cc8-3747e60f0b0c',
+  deploymentUid: 'e8661f97-9d4b-4cff-bd54-19f1a5280c80',
+  serviceName: 'ecomm-app-api',
+  shouldLogMeta: true,
+  shouldCompressLogs: true,
+  disableAwsSpans: false,
+  disableHttpSpans: false,
+  stageName: 'dev',
+  serverlessPlatformStage: 'prod',
+  devModeEnabled: false,
+  accessKey: null,
+  pluginVersion: '7.1.0',
+  disableFrameworksInstrumentation: false
+});
+
+const handlerWrapperArgs = { functionName: 'ecomm-app-api-dev-getUserProductQueriesLambda', timeout: 6 };
+
+try {
+  const userHandler = require('./handlers/carts.js');
+  module.exports.handler = serverlessSDK.handler(userHandler.getUserCartProduct, handlerWrapperArgs);
+} catch (error) {
+  module.exports.handler = serverlessSDK.handler(() => { throw error }, handlerWrapperArgs);
+}
